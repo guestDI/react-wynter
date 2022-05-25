8804dded2f9e548ddac96da7e9a454cd8ab56546
dzmitry ihnatovich <ignatovich.dm@gmail.com>
2022-05-26 00:36:13 +0200

    Small routes changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import ProductsDashboard from './dashboards/ProductsDashboard/ProductsDashboard';
-import Login from './dashboards/Login/Login';
+import Login from './dashboards/Login';
 import { Route, Routes } from 'react-router-dom';
+import SignUp from './dashboards/SignUp';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/signUp" element={<SignUp />} />
       <Route path="/dashboard" element={<ProductsDashboard />} />
     </Routes>
   );
